1c6786bc94c6bebf20150cfe303fca8bc1c4c17c
Gustavo Reis <henrique.gustavoreis@gmail.com>
2019-08-28 21:32:58 -0300

    upload files feature

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,12 @@
 module.exports = {
 	dialect: 'postgres',
 	host: process.env.DB_HOST,
-	username: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_NAME,
+	// username: process.env.DB_USER,
+	// password: process.env.DB_PASS,
+	// database: process.env.DB_NAME,
+	username: 'postgres',
+	password: 'docker',
+	database: 'meetapp',
 	define: {
 		timestamps: true,
 		underscored: true,
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', (req, res) => {
 	return res.json({ ok: true });
 });
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;

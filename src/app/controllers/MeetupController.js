7e782c4f1b4d4cd01566b4249d3992827e82d05a
Gustavo Reis <henrique.gustavoreis@gmail.com>
2019-08-28 21:40:28 -0300

    migrations meetups and subscriptions && meetup base controller

---
+++ src/app/controllers/MeetupController.js
@@ -0,0 +1,15 @@
+class MeetupController {
+	async store(req, res) {
+		return res.json({ ok: true });
+	}
+
+	async update(req, res) {
+		return res.json({ ok: true });
+	}
+
+	async delete(req, res) {
+		return res.json({ ok: true });
+	}
+}
+
+export default new MeetupController();
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,5 +18,9 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/meetups', MeetupController.store);
+routes.put('/meetups', MeetupController.update);
+routes.delete('/meetups', MeetupController.delete);
 
 export default routes;

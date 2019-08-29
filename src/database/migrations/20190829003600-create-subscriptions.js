be9249ac2cf43cdd8234307a157773469dc80319
Gustavo Reis <henrique.gustavoreis@gmail.com>
2019-08-28 21:49:33 -0300

    migrations meetups and subscriptions done

--- src/database/migrations/20190829003600-create-subscriptions.js
+++ src/database/migrations/20190829003600-create-subscriptions.js
@@ -1,23 +1,44 @@
-'use strict';
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('subscriptions', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			user_id: {
+				type: Sequelize.INTEGER,
+				references: {
+					model: 'users',
+					key: 'id',
+					onUpdate: 'CASCADE',
+					onDelete: 'CASCADE',
+					allowNull: false,
+				},
+			},
+			meetup_id: {
+				type: Sequelize.INTEGER,
+				references: {
+					model: 'meetups',
+					key: 'id',
+					onUpdate: 'CASCADE',
+					onDelete: 'CASCADE',
+					allowNull: false,
+				},
+			},
+			created_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			updated_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+		});
+	},
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-  },
-
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+	down: queryInterface => {
+		return queryInterface.dropTable('subscriptions');
+	},
 };

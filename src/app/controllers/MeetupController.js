3ea6b803675a82ad4b6b86734264d8945e3c0240
gustavopradoreis <henrique.gustavoreis@gmail.com>
2019-08-30 14:30:56 -0300

    custom validations in meetups

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,10 +1,17 @@
 import * as Yup from 'yup';
+import { isBefore, parseISO } from 'date-fns';
 import Meetup from '../models/Meetup';
+import File from '../models/File';
+import User from '../models/User';
 
 class MeetupController {
 	async index(req, res) {
 		const meetups = await Meetup.findAll({
 			where: { user_id: req.userId },
+			include: [
+				{ model: File, as: 'banner', attributes: ['name', 'path', 'url'] },
+				{ model: User, as: 'user', attributes: ['id', 'name'] },
+			],
 		});
 
 		return res.json(meetups);
@@ -21,6 +28,10 @@ class MeetupController {
 
 		if (!(await schema.isValid(req.body))) {
 			return res.status(400).json({ error: 'Validation fails!' });
+		}
+
+		if (isBefore(parseISO(req.body.date), new Date())) {
+			return res.status(400).json({ error: 'Meetup date is before today' });
 		}
 
 		const meetup = await Meetup.create({
@@ -49,6 +60,20 @@ class MeetupController {
 			return res.status(400).json({ error: 'Meetup not found!' });
 		}
 
+		if (meetup.user_id !== req.userId) {
+			return res
+				.status(401)
+				.json({ error: 'You can only update yours meetups' });
+		}
+
+		if (isBefore(parseISO(req.body.date), new Date())) {
+			return res.status(400).json({ error: 'Meetup date is before today' });
+		}
+
+		if (meetup.past) {
+			return res.status(400).json({ error: "You can't update past meetups!" });
+		}
+
 		const updatedMeetup = await meetup.update(req.body);
 
 		return res.json(updatedMeetup);
@@ -58,6 +83,16 @@ class MeetupController {
 		const meetup = await Meetup.findByPk(req.params.id);
 		if (!meetup) {
 			return res.status(400).json({ error: 'Meetup not found!' });
+		}
+
+		if (meetup.user_id !== req.userId) {
+			return res
+				.status(401)
+				.json({ error: 'You can only delete yours meetups' });
+		}
+
+		if (meetup.past) {
+			return res.status(400).json({ error: "You can't delete past meetups!" });
 		}
 
 		await meetup.destroy();
